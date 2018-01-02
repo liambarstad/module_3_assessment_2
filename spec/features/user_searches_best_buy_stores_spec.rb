7d4063e4eb2e655ad56559dcb54d60954b23482f
Liam Barstad <liambarstad@gmail.com>
2018-01-02 11:55:43 -0700

    remove comments

--- spec/features/user_searches_best_buy_stores_spec.rb
+++ spec/features/user_searches_best_buy_stores_spec.rb
@@ -1,14 +1,3 @@
-#And I should see pagination links below the search results for the number of pages (2 in this case)
-#And `1` is my current page but isn't a clickable link
-#And `2` is a clickable link
-#When I click `2`
-#Then I should be taken to the next page of search results
-#Then my current path should be "/search" (ignoring params)
-#And in the params I should see `page=2`
-#And I should see stores within 25 miles
-#And I should see a message that says "17 Total Stores"
-#And I should see the next 6 results
-#And I should see the long name, city, distance, phone number and store type for each of the next 6 results
 require 'rails_helper'
 
 RSpec.feature "user searches stores" do
